3358e1798b80c248cb182fe053e6a30772c89b81
Susan Mills <juliadawn.gs@gmail.com>
2020-10-07 17:08:30 -0700

    Create initial implementation

--- src/edu/mills/cs180/PalindromeChecker.java
+++ src/edu/mills/cs180/PalindromeChecker.java
@@ -16,6 +16,9 @@ public class PalindromeChecker {
      * @return true if the string is a palindrome, false otherwise
      */
     public static boolean isPalindrome(String s) {
-        return false;
+        // For now, assume string has no non-alphanumeric characters passed in.
+    	StringBuilder sb = new StringBuilder(s);
+    	sb.reverse();
+    	return s.equalsIgnoreCase(sb.toString());
     }
 }
