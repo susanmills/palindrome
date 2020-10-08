560c01061615509ca6c67b68608e33e0dfb85eed
Susan Mills <juliadawn.gs@gmail.com>
2020-10-07 17:47:14 -0700

    Remove outdated comment

--- src/edu/mills/cs180/PalindromeChecker.java
+++ src/edu/mills/cs180/PalindromeChecker.java
@@ -16,7 +16,6 @@ public class PalindromeChecker {
      * @return true if the string is a palindrome, false otherwise
      */
     public static boolean isPalindrome(String s) {
-        // For now, assume string has no non-alphanumeric characters passed in.
         s = s.replaceAll("[^A-Za-z0-9]", "");
     	StringBuilder sb = new StringBuilder(s);
     	sb.reverse();
